c6fdf09f2ccc24d95c1ce1d8332f696deef00503
Lorrane Idevic Gonçalves <lorraneidevic@MacBook-Pro-de-Lorrane.local>
2018-05-02 10:54:39 -0300

    Show Menu method

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,16 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("\n------------------ Welcome to Biblioteca - Bangalore Public Library ------------------");
+
+        ShowMenu();
+    }
+
+    private static void ShowMenu(){
+        System.out.println("\nPlease choose one of the options in the menu below by typing its number:");
+        System.out.println("1 - List Books");
+        System.out.println("2 - Checkout Book");
+        System.out.println("3 - Return Book");
+        System.out.println("4 - Quit");
     }
 }
